762bc7c36b86009cf0e9d81cd8aafb8175a8b27a
modigliani-A <138287978+modigliani-A@users.noreply.github.com>
2023-12-27 09:41:13 -0600

    Formato de imagen

--- js/main.js
+++ js/main.js
@@ -27,43 +27,43 @@ document.getElementById('validarBtn').addEventListener('click', function() {
         document.getElementById('passwordInput').style.display = 'none';
         document.getElementById('validarBtn').style.display = 'none';
         document.getElementById('nombre').innerText="Adled Snèdmazfuk";
-        document.getElementById('icon').src="img/Adled.png";
+        document.getElementById('icon').src="img/Adled.webp";
         document.getElementById('modal').style.display = 'block';
     } else if(password === '2512') {
         document.getElementById('passwordInput').style.display = 'none';
         document.getElementById('validarBtn').style.display = 'none';
         document.getElementById('nombre').innerText="Ángel Benedetti";
-        document.getElementById('icon').src="img/Angel.png";
+        document.getElementById('icon').src="img/Angel.webp";
         document.getElementById('modal').style.display = 'block';
     } else if(password === '6245') {
         document.getElementById('passwordInput').style.display = 'none';
         document.getElementById('validarBtn').style.display = 'none';
         document.getElementById('nombre').innerText="Antoine Hervé";
-        document.getElementById('icon').src="img/Rhadav.png";
+        document.getElementById('icon').src="img/Rhadav.webp";
         document.getElementById('modal').style.display = 'block';
     } else if(password === '1111') {
         document.getElementById('passwordInput').style.display = 'none';
         document.getElementById('validarBtn').style.display = 'none';
         document.getElementById('nombre').innerText="Asteriąn Snèdmazfuk";
-        document.getElementById('icon').src="img/Jae.png";
+        document.getElementById('icon').src="img/Jae.webp";
         document.getElementById('modal').style.display = 'block';
     } else if(password === '1313') {
         document.getElementById('passwordInput').style.display = 'none';
         document.getElementById('validarBtn').style.display = 'none';
         document.getElementById('nombre').innerText="Damian Astaroth";
-        document.getElementById('icon').src="img/Dami.png";
+        document.getElementById('icon').src="img/Dami.webp";
         document.getElementById('modal').style.display = 'block';
     } else if(password === '2113') {
         document.getElementById('passwordInput').style.display = 'none';
         document.getElementById('validarBtn').style.display = 'none';
         document.getElementById('nombre').innerText="Dominic";
-        document.getElementById('icon').src="img/Domi.png";
+        document.getElementById('icon').src="img/Domi.webp";
         document.getElementById('modal').style.display = 'block';
     } else if(password === '9231') {
         document.getElementById('passwordInput').style.display = 'none';
         document.getElementById('validarBtn').style.display = 'none';
         document.getElementById('nombre').innerText="Judas";
-        document.getElementById('icon').src="img/Judas.png";
+        document.getElementById('icon').src="img/Judas.webp";
         document.getElementById('modal').style.display = 'block';
     } else if(password === '6666') {
         document.getElementById('passwordInput').style.display = 'none';
@@ -75,37 +75,37 @@ document.getElementById('validarBtn').addEventListener('click', function() {
         document.getElementById('passwordInput').style.display = 'none';
         document.getElementById('validarBtn').style.display = 'none';
         document.getElementById('nombre').innerText="Gian Luca";
-        document.getElementById('icon').src="img/Luqi.png";
+        document.getElementById('icon').src="img/Luqi.webp";
         document.getElementById('modal').style.display = 'block';
     } else if(password === '2133') {
         document.getElementById('passwordInput').style.display = 'none';
         document.getElementById('validarBtn').style.display = 'none';
         document.getElementById('nombre').innerText="Mattías Voruz";
-        document.getElementById('icon').src="img/Mati.png";
+        document.getElementById('icon').src="img/Mati.webp";
         document.getElementById('modal').style.display = 'block';
     } else if(password === '5555') {
         document.getElementById('passwordInput').style.display = 'none';
         document.getElementById('validarBtn').style.display = 'none';
         document.getElementById('nombre').innerText="Miuccia Ferretti";
-        document.getElementById('icon').src="img/Miu.png";
+        document.getElementById('icon').src="img/Miu.webp";
         document.getElementById('modal').style.display = 'block';
     } else if(password === '5326') {
         document.getElementById('passwordInput').style.display = 'none';
         document.getElementById('validarBtn').style.display = 'none';
         document.getElementById('nombre').innerText="Santiago";
-        document.getElementById('icon').src="img/Santi.png";
+        document.getElementById('icon').src="img/Santi.webp";
         document.getElementById('modal').style.display = 'block';
     } else if(password === '1243') {
         document.getElementById('passwordInput').style.display = 'none';
         document.getElementById('validarBtn').style.display = 'none';
         document.getElementById('nombre').innerText="Wren S. Hellsing";
-        document.getElementById('icon').src="img/Wren.png";
+        document.getElementById('icon').src="img/Wren.webp";
         document.getElementById('modal').style.display = 'block';
     } else if(password === '2121') {
         document.getElementById('passwordInput').style.display = 'none';
         document.getElementById('validarBtn').style.display = 'none';
         document.getElementById('nombre').innerText="變精";
-        document.getElementById('icon').src="img/Aki.png";
+        document.getElementById('icon').src="img/Aki.webp";
         document.getElementById('modal').style.display = 'block';
     } else {
         alert('Contraseña incorrecta');
